cc7ad14b1953c33b73039b6fa3dba6a0b1977c35
Cichanowski Miguel <miguel.cicha@gmail.com>
2023-02-06 20:35:52 -0300

    clase 39

---
+++ workspace-c39/silicon-app/src/Home.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import Menu from "./Menu";
+
+function Home() {
+    return <>
+        <Menu />
+        Pagina de inicio
+    </>;
+}
+
+export default Home;
---
+++ workspace-c39/silicon-app/src/persona/PersonaList.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import Menu from "../Menu";
+
+function PersonaList() {
+    return <>
+   <Menu />
+      PersonaList
+    </>
+}
+
+export default PersonaList;
