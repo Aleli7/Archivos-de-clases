810696d24dcea2c053a0916b3c531ba4064d59e9
Cichanowski Miguel <miguel.cicha@gmail.com>
2023-02-14 08:12:17 -0300

    clase 41

---
+++ workspace-c41/silicon-app/src/About.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import Menu from "./Menu";
+
+function About() {
+    return <>
+        Pagina About. Info adicional.
+    </>
+}
+
+export default About;
